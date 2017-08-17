ececc15412fbdd45cc90043a90fd2df10212f433
shengjie <liushengchieh@gmail.com>
2017-08-17 15:06:29 +0800

    新增切换城市和手动更新天气的功能

--- app/src/main/java/com/hengweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/hengweather/android/ChooseAreaFragment.java
@@ -22,6 +22,7 @@ import android.widget.Toast;
 import com.hengweather.android.db.City;
 import com.hengweather.android.db.County;
 import com.hengweather.android.db.Province;
+import com.hengweather.android.gson.Weather;
 import com.hengweather.android.util.HttpUtil;
 import com.hengweather.android.util.Utility;
 
@@ -119,10 +120,18 @@ public class ChooseAreaFragment extends Fragment {
                     queryCounties();
                 } else if (currentLevel == LEVEL_COUNTY) {
                     String weatherId = countyList.get(position).getWeatherId();
-                    Intent intent = new Intent(getActivity(), WeatherActivity.class);
-                    intent.putExtra("weather_id", weatherId);
-                    startActivity(intent);
-                    getActivity().finish();
+                    if (getActivity() instanceof MainActivity) {
+                        Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                        intent.putExtra("weather_id", weatherId);
+                        startActivity(intent);
+                        getActivity().finish();
+                    } else if (getActivity() instanceof WeatherActivity) {
+                        WeatherActivity activity = (WeatherActivity) getActivity();
+                        activity.drawerLayout.closeDrawers();
+                        activity.swipeRefresh.setRefreshing(true);
+                        activity.requestWeather(weatherId);
+                    }
+
                 }
             }
         });
